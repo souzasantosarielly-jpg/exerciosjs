ac3cc06e127585724decd413d36371bb8214d6ee
ari <souza.santos.arielly@escola.pr.gov.br>
2026-04-10 17:23:10 -0300

    Add files via upload

---
+++ jspuro/ex02.js
@@ -0,0 +1,36 @@
+//verificar se um número é maior que 18
+let num =15;
+if (num >10) {
+console.log(`o numero ${num} é maior que 10`);
+}
+
+//verifique se um número é par ou ímpar
+
+let num2= 8;
+if (num % 2 ===0){
+console.log("par")
+}
+
+//verificar se um número é positivo ou negativo
+let num3 =5;
+if (num3 >= 0){
+    console.log('positivo') 
+} else {
+    console.log('negativo')
+}
+
+let temp= 15;
+if (temp <= 15) {
+    console.log('frio')
+} else if (temp >=16 && temp <=25) {
+    console.log(agradável)
+} else {
+    console.log('quente')
+}
+let idade =12;
+if(idade < 12) {
+    console.log('adoslescente')
+} else {
+    console.log('Adulto')
+}
+
---
+++ jspuro/script.js
@@ -0,0 +1,16 @@
+// let, const, console.log
+
+let nome ='ari';//string
+let numero ='25'; //string
+let numero2= 25;//int
+let numero3 = 25.12 //number, float
+
+let_num= 12;
+
+nome= 'Maria';
+
+const pi = 3,14;
+pi = 3,15
+console.log(PI);
+let verdadeiro = true; // boolleano
+let falso = falso; // booleano
